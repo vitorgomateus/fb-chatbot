b8e0d909cdf8164f61e4f49c949639e08f068bf3
vitorgomateus <vitorg8.5@gmail.com>
2018-03-02 18:38:04 +0000

    getting echofeedback

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ def webhook():
     # endpoint for processing incoming messaging events
 
     data = request.get_json()
-    log("DATADATADATADATADATADATADATADATA")
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
     if data["object"] == "page":
