0ee7d093f0a9b39e08395c8b4fe1b14adfdf11c2
vitorgomateus <vitorg8.5@gmail.com>
2018-03-05 22:47:27 +0000

    handover protocol study

--- app.py
+++ app.py
@@ -62,16 +62,18 @@ def webhook():
                     sender_id_pass = messaging_event["sender"]["id"]
 
                     if messaging_event.get("message"):  # someone sent us a message
-                        pass
-                        #sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                        #recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                       # message_text = messaging_event["message"]["text"]  # the message's text
+                        sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                        recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                        message_text = messaging_event["message"]["text"]  # the message's text
 
-                        #send_message(sender_id, "I'm okay?")
+                        send_message(sender_id, "BOT :D")
 
                     if messaging_event.get("request_thread_control"):  # ADMIN requested control
                         log("ADMIN REQUEST CONTROL sender_id={sendr}".format(sendr=sender_id_pass))
                         pass_thread_control(sender_id_pass)
+
+                    if messaging_event.get("request_thread_control"):  # ADMIN control passed
+                        log("PASS CONTROL TO ADMIN")
                         
                     if messaging_event.get("delivery"):  # delivery confirmation
                         pass
@@ -88,7 +90,7 @@ def webhook():
 def send_message(recipient_id, message_text):
 
 #       intact -v-
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+    log("SENDing message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
@@ -97,6 +99,28 @@ def send_message(recipient_id, message_text):
         "Content-Type": "application/json"
     }
     data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message":{
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+def send_webview(title_arr, img_arr, url_arr)
+    log("SEND_WEBVIEW")
+    # iterate through arr...
+    paramsWebview = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headersWebview = {
+        "Content-Type": "application/json"
+    }
+    dataWebview = json.dumps({
         "recipient": {
             "id": recipient_id
         },
@@ -141,10 +165,10 @@ def send_message(recipient_id, message_text):
                 ]
               }
             }
-  }
+        }
     })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
+    # r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=paramsWebview, headers=headersWebview, data=dataWebview)
+    # if r.status_code != 200:
         log(r.status_code)
         log(r.text)
 
