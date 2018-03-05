6908221d291af25feb4b944b742c4a4bc0ae249a
vitorgomateus <vitorg8.5@gmail.com>
2018-03-05 13:28:48 +0000

    handover protocol study

--- app.py
+++ app.py
@@ -44,12 +44,12 @@ def webhook():
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
-
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    pass
+                    #sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    #recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                    # message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_message(sender_id, "I'm okay?")
+                    #send_message(sender_id, "I'm okay?")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
