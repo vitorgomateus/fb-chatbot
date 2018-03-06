932960b88db7d9ebe0dee4b127b2665bf4b279e0
vitorgomateus <vitorg8.5@gmail.com>
2018-03-06 17:30:23 +0000

    woocommerce products http request study

--- app.py
+++ app.py
@@ -65,11 +65,11 @@ def webhook():
                     if messaging_event.get("message"):  # someone sent us a message
                         sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                         recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                        message_text = messaging_event["message"]["text"]  # the message's text
 
                         if messaging_event["message"].get("attachments"):
                             log("Bot got a sticker!")
                         else:
+                            message_text = messaging_event["message"]["text"]  # the message's text
                             if message_text == "produtos":
                                 get_send_products(0)
                             else:
