2147767732af188483bcd30163e1c08a1340978e
vitorgomateus <vitorg8.5@gmail.com>
2018-03-10 17:01:34 +0000

    WC REST APIstudy

--- app.py
+++ app.py
@@ -253,8 +253,8 @@ def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
         if type(msg) is dict:
             msg = json.dumps(msg)
         else:
-            msg = unicode(msg).format(*args, **kwargs)
-            #msg="Oh, well.."
+            #msg = unicode(msg).format(*args, **kwargs)
+            msg="Oh, well.."
         print u"{}: {}".format(datetime.now(), msg)
     except UnicodeEncodeError:
         pass  # squash logging errors in case of non-ascii text
