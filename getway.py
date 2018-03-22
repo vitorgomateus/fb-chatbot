19bdc58af118aaf6fc12843c143c8d389aa3583a
vitorgomateus <vitorg8.5@gmail.com>
2018-03-22 17:53:31 +0000

    control image size in webview

--- getway.py
+++ getway.py
@@ -36,21 +36,18 @@ def get_send_products(category, recipient):
 
     arr_title=[response_products[0]["name"], response_products[1]["name"], response_products[2]["name"]]
     
-    arr_image=[response_products[0]["images"][0]["src"], response_products[1]["images"][0]["src"], response_products[2]["images"][0]["src"]]
-    
-   # arr_link= [response_products[0]["permalink"], response_products[1]["permalink"], response_products[2]["permalink"]]
-    arr_link= [string.replace(response_products[0]["permalink"], ".jpg", "-300x300.jpg"), 
-        string.replace(response_products[1]["permalink"], ".jpg", "-300x300.jpg"), 
-        string.replace(response_products[0]["permalink"], ".jpg", "-300x300.jpg")
+    #arr_image=[response_products[0]["images"][0]["src"], response_products[1]["images"][0]["src"], response_products[2]["images"][0]["src"]]
+    arr_image= [string.replace(response_products[0]["images"][0]["src"], ".jpg", "-300x300.jpg"), 
+        string.replace(response_products[1]["images"][0]["src"], ".jpg", "-300x300.jpg"), 
+        string.replace(response_products[0]["images"][0]["src"], ".jpg", "-300x300.jpg")
     ]
-    logar("string from response.json()")
-    logar(response_products[0]["permalink"])
-    logar("string replace")
-    logar(string.replace(response_products[0]["permalink"], ".jpg", "-300x300.jpg"))
-    bloooooo=response_products[0]["permalink"]
-    bllaaaaa=string.replace(bloooooo, ".jpg", "-300x300.jpg")
-    logar("blooobllalala")
-    logar(bllaaaaa)
+
+    arr_link= [response_products[0]["permalink"], response_products[1]["permalink"], response_products[2]["permalink"]]
+    
+    logar("arr_title")
+    logar(arr_title)
+    logar("arr_image")
+    logar(arr_image)
     logar("arr_link")
     logar(arr_link)
 
