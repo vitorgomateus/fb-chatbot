bdf61f4de0d93bba0ea2029a01a4aa74599db491
vitorgomateus <vitorg8.5@gmail.com>
2018-03-22 15:30:10 +0000

    simplify code

--- funcao.py
+++ funcao.py
@@ -8,7 +8,7 @@ from util import logar
 
 def senda_message(recipient_id, message_text):
 
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+    logar("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
@@ -26,5 +26,5 @@ def senda_message(recipient_id, message_text):
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
+        logar(r.status_code)
+        logar(r.text)
