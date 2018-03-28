04c9b556677764fa762eaebf5e18f211a1f9cf3c
vitorgomateus <vitorg8.5@gmail.com>
2018-03-28 18:00:58 +0100

    fix error timeout

--- outway.py
+++ outway.py
@@ -7,14 +7,14 @@ from util import logar
 #                message_text = if isWebView ? 0 : text
 def send_message(message_text, recipient_id, title_arr, img_arr, url_arr):
     send_object = "there seems to be an error"
-    logar("title_arr")
-    logar(title_arr[1])
-    logar(title_arr)
 
     if(message_text==0):
     #                                                     -------------------------------Web View
         logar("SEND_WEBVIEW")
 
+    logar("title_arr")
+    logar(title_arr[1])
+    logar(title_arr)
         element = {}
         arr_elements = []
         #if(!(len(title_arr)==len(img_arr)==len(url_arr))):
